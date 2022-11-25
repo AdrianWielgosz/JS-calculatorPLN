0c216ed7b8891f05587b223c5626d36b5b894ff4
Adrian <adi9918@onet.pl>
2022-11-25 16:33:07 +0100

    second commit

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const result = document.getElementById("result");
 const amount = document.getElementById("amount");
 
 convert.addEventListener("click", function () {
-
-	fetch(`http://api.nbp.pl/api/exchangerates/tables/a/`)
+	fetch(`https://api.nbp.pl/api/exchangerates/tables/a/`)
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			const rate = data[0].rates;
-			const selectedRate = rate.filter((item) =>
-					item.code === document.querySelector("#from").value
-				)
-			const total = amount.value * selectedRate[0].mid;
+			const rates = data[0].rates;
+			const selectedRate = rates.find(
+				(el) => el.code === document.querySelector("#from").value
+			);
+
+			const total = amount.value * selectedRate.mid;
+			if (amount.value <= 0) {
+				return alert("musisz podać liczbę większą od zera!");
+			}
 			result.innerHTML = ` to ${total.toFixed(2)} PLN`;
 		});
 });
