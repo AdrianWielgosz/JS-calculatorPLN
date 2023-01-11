69e2fd787b2321fb47490d1a242c8b0edf2d70b8
Adrian <adi9918@onet.pl>
2023-01-11 11:45:54 +0100

    rwd

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const convert = document.getElementById("convert");
 const result = document.getElementById("result");
 const amount = document.getElementById("amount");
@@ -21,27 +20,3 @@ convert.addEventListener("click", function () {
 			result.innerHTML = ` to ${total.toFixed(2)} PLN`;
 		});
 });
-=======
-const convert = document.getElementById("convert");
-const result = document.getElementById("result");
-const amount = document.getElementById("amount");
-
-convert.addEventListener("click", function () {
-	fetch(`https://api.nbp.pl/api/exchangerates/tables/a/`)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			const rates = data[0].rates;
-			const selectedRate = rates.find(
-				(el) => el.code === document.querySelector("#from").value
-			);
-
-			const total = amount.value * selectedRate.mid;
-			if (amount.value <= 0) {
-				return alert("musisz podać liczbę większą od zera!");
-			}
-			result.innerHTML = ` to ${total.toFixed(2)} PLN`;
-		});
-});
->>>>>>> 1134e4348cde7c9ae54e890bc7f0dcff107cdce3
